50d04b6fe98a2a7dbf091fc26c26d565650e6d4b
trentasaurus <50506351+trentasaurus@users.noreply.github.com>
2021-10-01 19:32:36 -0400

    Set up structure for local database

---
+++ Next Level/core/src/com/mygdx/nextlevel/db/sqlite/LevelInfoDB.java
@@ -0,0 +1,24 @@
+package com.mygdx.nextlevel.db.sqlite;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+/**
+ *
+ */
+public class LevelInfoDB {
+    /**
+     * Connect to database
+     */
+    public static void connect() {
+
+    }
+
+    /**
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        connect();
+    }
+}
