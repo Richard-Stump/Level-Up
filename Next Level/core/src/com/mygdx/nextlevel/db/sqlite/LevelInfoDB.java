719b319666b176f7eb75d5d1775935f128ddc709
trentasaurus <50506351+trentasaurus@users.noreply.github.com>
2021-10-01 19:58:31 -0400

    Made functions to access database

--- Next Level/core/src/com/mygdx/nextlevel/db/sqlite/LevelInfoDB.java
+++ Next Level/core/src/com/mygdx/nextlevel/db/sqlite/LevelInfoDB.java
@@ -1,7 +1,6 @@
 package com.mygdx.nextlevel.db.sqlite;
 
-import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  *
@@ -10,8 +9,46 @@ public class LevelInfoDB {
     /**
      * Connect to database
      */
-    public static void connect() {
+    public static void connect(String filename) {
+        Connection connection = null;
 
+        try {
+            String url = "jdbc:sqlite:" + filename;
+
+            connection = DriverManager.getConnection(url);
+            System.out.println("Connected");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                    System.out.println("Closed");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void createDatabase(String filename) {
+        String url = "jdbc:sqlite:" + filename;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection(url);
+
+            if (conn != null) {
+                DatabaseMetaData metaData = conn.getMetaData();
+                System.out.println("Driver name: " + metaData.getDriverName());
+                System.out.println("created");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -19,6 +56,7 @@ public class LevelInfoDB {
      * @param args command line arguments
      */
     public static void main(String[] args) {
-        connect();
+        createDatabase("example.db");
+        connect("example.db");
     }
 }
