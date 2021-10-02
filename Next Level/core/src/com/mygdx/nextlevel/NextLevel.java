bb3e97094d5f73a383f75689bb9a05e98dac7b52
Joseph Chen <54645109+jchen32@users.noreply.github.com>
2021-10-02 16:57:24 -0400

    Add files via upload
    tried a bunch of different ways to approach collision detection, just can't seem to get it to work

--- Next Level/core/src/com/mygdx/nextlevel/NextLevel.java
+++ Next Level/core/src/com/mygdx/nextlevel/NextLevel.java
@@ -1,28 +1,30 @@
 package com.mygdx.nextlevel;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.nextlevel.screens.GameScreen;
 
 public class NextLevel extends Game {
 	public SpriteBatch batch;
-	public static final int WIDTH = 800;
-	public static final int HEIGHT = 480;
+//	public static final int WIDTH = 800;
+//	public static final int HEIGHT = 480;
+	public Screen gameScreen;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		setScreen(new GameScreen(this));
+		gameScreen = new GameScreen();
+		setScreen(gameScreen);
 	}
 
-	@Override
-	public void render () {
-		super.render();
-	}
-
-	@Override
-	public void dispose () {
-		batch.dispose();
-	}
+//	@Override
+//	public void render () {
+//		super.render();
+//	}
+//
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//	}
 
 }
--- Next Level/core/src/com/mygdx/nextlevel/screens/GameScreen.java
+++ Next Level/core/src/com/mygdx/nextlevel/screens/GameScreen.java
@@ -1,16 +1,19 @@
 package com.mygdx.nextlevel.screens;
 
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -19,85 +22,107 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.nextlevel.NextLevel;
-import com.mygdx.nextlevel.scenes.Lives;
+//import com.mygdx.nextlevel.scenes.Lives;
 
 
 import java.util.Iterator;
 public class GameScreen implements Screen {
-    NextLevel game;
-    Texture heroImage;
-    Texture enemyImage;
-    Rectangle hero;
-    Rectangle enemy;
+//    NextLevel game;
+    Stage stage;
+    SpriteBatch batch;
+    Texture heroImage, enemyImage;
+    Rectangle hero, enemy;
     OrthographicCamera camera;
-    Viewport viewport;
-    Lives lives;
+    float heroImagex = 200, heroImagey = 20;
+    float enemyImagex = 400, enemyImagey = 20;
+//    Viewport viewport;
+//    Lives lives;
 
-    private World world;
-    private Box2DDebugRenderer debugRenderer;
+    World world;
+//    private Box2DDebugRenderer debugRenderer;
 
 
-    public GameScreen(NextLevel game) {
-        this.game = game;
-        enemyImage = new Texture(Gdx.files.internal("boss-sprite.jpeg"));
-        heroImage = new Texture(Gdx.files.internal("hero.png"));
-        camera = new OrthographicCamera();
-        viewport = new FitViewport(NextLevel.WIDTH,NextLevel.HEIGHT, camera);
-        lives = new Lives(game.batch);
-        camera.setToOrtho(false, 800, 480);
-        enemy = new Rectangle();
-        hero = new Rectangle();
-        hero.x = 200;
-        hero.y = 20;
-        hero.width = 64;
-        hero.height = 64;
-        enemy.x = 400;
-        enemy.y = 20;
-        enemy.width = 15;
-        enemy.height = 15;
+//    public GameScreen(NextLevel game) {
+//        this.game = game;
+//        enemyImage = new Texture(Gdx.files.internal("enemy.png"));
+//        heroImage = new Texture(Gdx.files.internal("hero.png"));
+//        camera = new OrthographicCamera();
+////        viewport = new FitViewport(NextLevel.WIDTH,NextLevel.HEIGHT, camera);
+////        lives = new Lives(game.batch);
+//        camera.setToOrtho(false, 800, 480);
+//        enemy = new Rectangle();
+//        hero = new Rectangle();
+//        hero.x = 200;
+//        hero.y = 20;
+//        hero.width = 100;
+//        hero.height = 100;
+//        enemy.x = 600;
+//        enemy.y = 20;
+//        enemy.width = 100;
+//        enemy.height = 100;
+//        Body body;
+//        world = new World(new Vector2(0,-1f), true);
+////        debugRenderer = new Box2DDebugRenderer();
+////        BodyDef = new BodyDef();
+//        BodyDef bdef = new BodyDef();
+//        bdef.type = BodyDef.BodyType.StaticBody;
+//        bdef.position.set(enemy.getX(), enemy.getY() );
+//        body = world.createBody(bdef);
+//        PolygonShape pShape = new PolygonShape();
+//        pShape.setAsBox(enemy.getWidth(), enemy.getHeight());
+//        FixtureDef fdef = new FixtureDef();
+//        fdef.shape = pShape;
+////        fdef.filter.categoryBits =
+//        body.createFixture(fdef);
+//
+//    }
 
-        world = new World(new Vector2(0,0), true);
-        debugRenderer = new Box2DDebugRenderer();
-        BodyDef bdef = new BodyDef();
-        PolygonShape shape = new PolygonShape();
-        FixtureDef fdef = new FixtureDef();
-        Body body;
-        bdef.type = BodyDef.BodyType.StaticBody;
-        bdef.position.set(enemy.getX(), enemy.getY() );
-        body = world.createBody(bdef);
-        shape.setAsBox(enemy.getWidth(), enemy.getHeight());
-        fdef.shape = shape;
-        body.createFixture(fdef);
-
-    }
-
-    public void update(float dt) {
-        world.step(1/60f, 6, 2);
-    }
+//    public void update(float dt) {
+//        world.step(1/60f, 6, 2);
+//    }
 
     @Override
     public void render(float delta) {
-//        ScreenUtils.clear(0, 0, 0.2f, 1);
+        Gdx.gl.glClearColor(1,1,1, 0);
+        ScreenUtils.clear(1,1,1,0);
+        batch.begin();
+        stage.draw();
+        batch.draw(heroImage, heroImagex, heroImagey, 50, 50);
+        batch.draw(enemyImage, 400, 20, 50, 50);
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            heroImagex -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            heroImagex += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.UP))
+            heroImagey += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
+            heroImagey -= 200 * Gdx.graphics.getDeltaTime();
+        batch.end();
+        hero = new Rectangle(heroImagex, heroImagey, 10,10);
+        enemy = new Rectangle(400, 20, 10,10);
+        if (hero.overlaps(enemy) && heroImagey > enemyImagey && heroImagex > enemyImagex && heroImagey < enemyImagey) {
+            System.out.println("Enemy has been killed.");
+        } else if (hero.overlaps(enemy)) {
+            System.out.println("Hero has been killed.");
+        }
 //        camera.update();
 //        game.batch.setProjectionMatrix(camera.combined);
 //        game.batch.begin();
-//        game.font.draw(game.batch, "Lives: " + lives, 0, 480);
+////        game.font.draw(game.batch, "Lives: " + lives, 0, 480);
 //        game.batch.draw(enemyImage, enemy.x, enemy.y, enemy.width, enemy.height);
 //        game.batch.draw(heroImage, hero.x, hero.y, hero.width, hero.height);
 //        game.batch.end();
 //
-//        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
-//            hero.x -= 200 * Gdx.graphics.getDeltaTime();
-//        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-//            hero.x += 200 * Gdx.graphics.getDeltaTime();
-//        if (Gdx.input.isKeyJustPressed(Input.Keys.UP))
-//            hero.y += 800 * Gdx.graphics.getDeltaTime();
-////        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
-////            hero.y -= 400 * Gdx.graphics.getDeltaTime();
 //        if (hero.x < 0)
 //            hero.x = 0;
 //        if (hero.x > 800 - 64)
 //            hero.x = 800 - 64;
+//        if (hero.overlaps(enemy)) {
+//            game.batch.begin();
+//            game.batch.draw(enemyImage, enemy.x, enemy.y, enemy.width, enemy.height);
+//            game.batch.draw(heroImage, hero.x, hero.y, hero.width, hero.height);
+//            game.batch.end();
+//        }
 //        if (hero.overlaps(enemy) && hero.y > enemy.y) {
 //            game.batch.begin();
 //            hero.x = 10;
@@ -123,23 +148,29 @@ public class GameScreen implements Screen {
 //            game.batch.draw(heroImage, hero.x, hero.y, hero.width, hero.height);
 //            game.batch.end();
 //        }
-        Gdx.gl.glClearColor(0,0,0,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        debugRenderer.render(world, camera.combined);
+//        Gdx.gl.glClearColor(0,0,0,1);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        /*debugRenderer.render(world, camera.combined);
         game.batch.setProjectionMatrix(lives.stage.getCamera().combined);
-        lives.stage.draw();
-        game.batch.begin();
-        game.batch.draw(enemyImage,10,10);
-        game.batch.end();
+        lives.stage.draw();*/
+//        game.batch.begin();
+//        game.batch.draw(enemyImage,10,10);
+//        game.batch.end();
     }
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+//        viewport.update(width, height);
     }
 
     @Override
     public void show() {
-
+        heroImage = new Texture("hero.png");
+        enemyImage = new Texture("enemy.png");
+        stage = new Stage();
+        hero = new Rectangle(heroImagex, heroImagey, 50,50);
+        enemy = new Rectangle(400, 20, 50,50);
+        Gdx.input.setInputProcessor(stage);
+        batch = new SpriteBatch();
     }
 
     @Override
