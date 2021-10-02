4ac2a68f10b1392ad270b46cf88bd0001182497c
trentasaurus <50506351+trentasaurus@users.noreply.github.com>
2021-10-02 14:38:18 -0400

    incorporated getConnection to get connection to database

--- Next Level/core/src/com/mygdx/nextlevel/DBController.java
+++ Next Level/core/src/com/mygdx/nextlevel/DBController.java
@@ -1,4 +1,20 @@
 package com.mygdx.nextlevel;
 
+import com.mygdx.nextlevel.dbUtil.DBConnection;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DBController {
+    private Connection connection;
+
+    public DBController() {
+        //connect to the database
+        try {
+            connection = DBConnection.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
