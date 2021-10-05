3ccd1be45d5b74ae4326d206542dd63b1d3215b2
Joseph Chen <54645109+jchen32@users.noreply.github.com>
2021-10-05 19:31:26 -0400

    Add files via upload
    began implementing collision with enemy/breakable blocks, fixing up player movement

---
+++ Level-Up-Nick/Next Level/desktop/src/com/mygdx/nextlevel/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.nextlevel.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.nextlevel.NextLevel;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NextLevel(), config);
+	}
+}
