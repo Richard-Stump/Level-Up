39748f443dcad276bf37c1a3954c3cde4dad31b0
Richard Stump <rnstump@purdue.edu>
2021-10-10 22:05:09 -0400

    Added TabbedPane to the level editor class

--- Next Level/core/src/com/mygdx/nextlevel/screens/EditLevelScreen.java
+++ Next Level/core/src/com/mygdx/nextlevel/screens/EditLevelScreen.java
@@ -5,20 +5,49 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.kotcrab.vis.ui.VisUI;
+import com.kotcrab.vis.ui.util.TableUtils;
+import com.kotcrab.vis.ui.widget.VisLabel;
+import com.kotcrab.vis.ui.widget.VisTable;
+import com.kotcrab.vis.ui.widget.VisWindow;
+import com.kotcrab.vis.ui.widget.tabbedpane.Tab;
+import com.kotcrab.vis.ui.widget.tabbedpane.TabbedPane;
+import com.kotcrab.vis.ui.widget.tabbedpane.TabbedPane.TabbedPaneStyle;
+import com.kotcrab.vis.ui.widget.tabbedpane.TabbedPaneAdapter;
+import com.kotcrab.vis.ui.widget.tabbedpane.TabbedPaneListener;
 import com.mygdx.nextlevel.NextLevel;
 import com.kotcrab.*;
 
-public class EditLevelScreen implements Screen, InputProcessor {
+import static com.badlogic.gdx.scenes.scene2d.ui.Cell.defaults;
+
+/* TODO: Make it so that the object TabbedPane always aligns to the left.
+ */
+
+public class EditLevelScreen implements Screen {
     private NextLevel game;
     private OrthographicCamera camera;
-    private ShapeRenderer shapeRenderer;
+    private SpriteBatch batch;
+    private TextureAtlas atlas;
+    private Skin skin;
+    private Stage stage;
+    private Viewport viewport;
     private int screenWidth, screenHeight;
-    private float pixelsPerTile;
 
-    private final Color backgroundColor = new Color(0.9f, 0.9f, 0.9f,1.0f);
+    private final Color backgroundColor = new Color(0.1f, 0.1f, 0.1f,1.0f);
     private final Color gridColor = new Color(0.2f, 0.2f, 0.2f,1.0f);
 
     public EditLevelScreen(NextLevel game) {
@@ -26,49 +55,58 @@ public class EditLevelScreen implements Screen, InputProcessor {
         this.screenWidth = Gdx.graphics.getWidth();
         this.screenHeight = Gdx.graphics.getHeight();
         this.camera = new OrthographicCamera(screenWidth, screenHeight);
-        this.shapeRenderer = new ShapeRenderer(1000);
 
-        pixelsPerTile = 64.0f;
+        this.atlas = new TextureAtlas(Gdx.files.internal("skin/neon-ui.atlas"));
+        this.skin = new Skin(Gdx.files.internal("skin/neon-ui.json"), atlas);
+
+
+        // Create a new orthographic camera and set it to view the center of the screen.
+        camera = new OrthographicCamera();
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        //Set up the viewport
+        viewport = new FitViewport(960, 500, camera);
+        viewport.apply();
+
+        batch = game.batch;
+        stage = new Stage(viewport, batch);
+
+        // For some reason the tabbed pane won't work with the other skin.
+        // TODO: Figure out how to use the same skin as the main menu
+        VisUI.load(VisUI.SkinScale.X1);
     }
 
 
     @Override
     public void show() {
         //Stage should control input:
-        Gdx.input.setInputProcessor(this);
+        Gdx.input.setInputProcessor(stage);
 
+        final VisTable table = new VisTable();
+        table.setFillParent(true);
+        table.add(new Label("Create Level", VisUI.getSkin())).center().expand();
+        table.row();
+
+        TabWindow win = new TabWindow();
+        stage.addActor(win);
+
+        stage.addActor(table);
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(backgroundColor);
-        camera.zoom = 20.0f;
 
-        renderGrid();
+        stage.act(delta);
+        stage.draw();
     }
-
-    private void renderGrid() {
-        shapeRenderer.begin(ShapeType.Line);
-        shapeRenderer.setColor(gridColor);
-
-        for(int y = 0; y <= (float)screenHeight; y++) {  
-            shapeRenderer.line(0, y, screenWidth, y);
-        }
-
-        for(int x = 0; x <= (float)screenHeight; x++) {
-            shapeRenderer.line(x, 0, x, screenHeight);
-        }
-
-        shapeRenderer.end();
-    }
-
 
     @Override
     public void resize(int width, int height) {
-        screenWidth = width;
-        screenHeight = height;
-        camera.viewportWidth = width;
-        camera.viewportHeight = height;
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
     }
 
     @Override
@@ -88,46 +126,70 @@ public class EditLevelScreen implements Screen, InputProcessor {
 
     @Override
     public void dispose() {
+        VisUI.dispose();
+    }
+}
+
+// This window class draws and handles the tabbed pane which will let players select
+// the various objects to place in their level.
+// This is incomplete!
+class TabWindow extends VisWindow {
+    public TabWindow() {
+        super("Level Objects:");
+        TableUtils.setSpacingDefaults(this);
+
+        setResizable(false);
+        setMovable(false);
+
+        // This table holds the contents of the tab we are currently looking at.
+        // This needs to be updated whenever a different tab is selected because the
+        // tab pane doesn't handle the content drawing for us.
+        final VisTable table = new VisTable();
+
+        TabbedPaneStyle style = VisUI.getSkin().get("default", TabbedPaneStyle.class);
+        final TabbedPane pane = new TabbedPane(style);
+
+        // listener to recreate the tab's content when a new tab is selected
+        pane.addListener(new TabbedPaneAdapter() {
+            @Override
+            public void switchedTab (Tab tab) {
+                table.clearChildren();
+                table.add(tab.getContentTable()).expand().fill();
+            }
+        });
+
+        add(pane.getTable()).expandX().fillX();
+        row();
+        add(table).expand().fill();
+
+        pane.add(new TestTab("Tiles"));
+        pane.add(new TestTab("Actors"));
+        pane.add(new TestTab("Enemies"));
+
+        setSize(300, 500);
+        setPosition(0, 0);
+    }
+}
+
+class TestTab extends Tab {
+    private String name;
+    private VisTable table;
+
+    public TestTab(String name) {
+        super(false, false);
+        this.name = name;
+
+        table = new VisTable();
+        table.add(new VisLabel(name));
     }
 
-
     @Override
-    public boolean keyDown(int keycode) {
-        return false;
+    public String getTabTitle() {
+        return name;
     }
 
     @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
+    public Table getContentTable() {
+        return table;
     }
 }
