3ee8445c8236d450b4b41d1198d8bf544185ddee
Christopher Hyman <chyman@purdue.edu>
2021-10-12 19:48:27 -0400

    Class Update
    Updated Tilemap.java by finishing init and initMap function... Working on parsing TileMap file

--- Next Level/core/src/com/mygdx/nextlevel/Tilemap.java
+++ Next Level/core/src/com/mygdx/nextlevel/Tilemap.java
@@ -1,8 +1,6 @@
 package com.mygdx.nextlevel;
 
-import com.badlogic.gdx.graphics.g2d.Batch;
 
-import java.util.ArrayList;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
@@ -22,20 +20,11 @@ public class Tilemap {
     private TiledMapTileLayer   mapLayer;
     private int                 mapWidth;
     private int                 mapHeight;
+    private int                 tileSize;
 
-    public Tilemap() {
-        tiles = new int[][] {
-                { 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 2, 2, 0, 0, 0 },
-                { 0, 0, 0, 2, 2, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 1, 1, 1, 1, 1, 1, 1, 1 },
-        };
-
-        init(null);
+    public Tilemap(final FileHandle tsConfigFile, final FileHandle tmFile) {
+        parseTileMapFile(tmFile);
+        init(tsConfigFile);
     }
 
     public TiledMap createMap(){
@@ -51,24 +40,60 @@ public class Tilemap {
         return map;
     }
 
+    public void parseTileMapFile(final FileHandle tmFile){
+        //Create tile array
+
+
+    }
+
     private void init(FileHandle tsConfigFile){
         //load config file
         final Json json = new Json();
+        final TileSetConfig config = json.fromJson(TileSetConfig.class, tsConfigFile);
 
+        final FileHandle tilesTexHandle = Gdx.files.internal(config.getTexPath());
+        if(!tilesTexHandle.exists() || tilesTexHandle.isDirectory()){
+            throw new IllegalArgumentException("Tileset Texture Path Invalid");
+        }
 
-        initMap();
+        tileSize = config.getTileSize();
+        if(tileSize != 64){
+            throw new IllegalArgumentException("Tile Size Invalid");
+        }
+
+        tilesTexture = new Texture(config.getTexPath());
+
+        try {
+            initMap();
+        } catch (Exception e){
+            tilesTexture.dispose();
+            throw e;
+        }
     }
 
     private void initMap(){
+        final TextureRegion[][] tileSplit = TextureRegion.split(tilesTexture, tileSize, tileSize);
+        final int rows = tileSplit.length;
+
+
+
         //Create a tileset
         tileSet = new TiledMapTileSet();
         int tileId = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < tileSplit[i].length; j++) {
+                final StaticTiledMapTile tile = new StaticTiledMapTile(tileSplit[i][j]);
+                tile.setId(tileId++);
+                tileSet.putTile(tile.getId(), tile);
+            }
+        }
 
 
         //Create an empty map
         map = new TiledMap();
         mapLayer = new TiledMapTileLayer(mapWidth, mapHeight, 64, 64);
         map.getLayers().add(mapLayer);
-
+        final Array<Texture> texts = Array.with(tilesTexture);
+        map.setOwnedResources(texts);
     }
 }
