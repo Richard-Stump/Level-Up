6da2876bfa024e8f028eeb3485b4e1e7925bd162
reeves34 <reeves34@purdue.edu>
2021-10-15 02:59:58 -0400

    Added JUnit tests for CreatedLevelsDB

---
+++ Next Level/core/src/com/mygdx/nextlevel/JUnitTests/CreatedLevelsDBTest.java
@@ -0,0 +1,98 @@
+package com.mygdx.nextlevel.JUnitTests;
+
+import com.mygdx.nextlevel.LevelInfo;
+import com.mygdx.nextlevel.dbHandlers.CreatedLevelsDB;
+import org.junit.*;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CreatedLevelsDBTest {
+    private static CreatedLevelsDB db;
+    private static List<LevelInfo> savedListCreated;
+
+    @BeforeClass
+    public static void savePriorDB() throws Throwable{
+        CreatedLevelsDB dbCreated = new CreatedLevelsDB();
+
+        if (dbCreated.isDBConnected()) {
+            savedListCreated = dbCreated.sortByTitle();
+
+            //clear database
+            for (LevelInfo level: savedListCreated) {
+                dbCreated.removeLevelInfo(level);
+            }
+        }
+        dbCreated.close();
+    }
+
+    @AfterClass
+    public static void restorePriorDB() throws Throwable {
+        CreatedLevelsDB dbCreated = new CreatedLevelsDB();
+
+        if (dbCreated.isDBConnected()) {
+            //restore items to database
+            for (LevelInfo levelInfo: savedListCreated) {
+                dbCreated.addLevelInfo(levelInfo);
+            }
+        }
+        dbCreated.close();
+    }
+
+    @Before
+    public void establishConnection() {
+        db = new CreatedLevelsDB();
+    }
+
+    @After
+    public void closeConnection() throws Throwable {
+        //clear database
+        for (LevelInfo level: db.sortByTitle()) {
+            db.removeLevelInfo(level);
+        }
+        db.close();
+    }
+
+    @Test
+    public void testIsUniqueIDEmptyDB() throws Throwable {
+        //Need in order to test
+        CreatedLevelsDB db = new CreatedLevelsDB();
+
+        //Conditions:
+        String id = "eeeeeeeeee";
+
+        //Get actual result
+        int actual = db.isUniqueID(id);
+
+        db.close();
+
+        //Set expected result
+        int expected = 1;
+
+        //test if result is what we expect
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsUniqueID1() throws Throwable {
+        //Need in order to test
+        CreatedLevelsDB db = new CreatedLevelsDB();
+
+        //Conditions:
+        String id = "sampleID13";
+        LevelInfo sameID = new LevelInfo(id, "Title", "author");
+        db.addLevelInfo(sameID);
+
+        //Get actual result
+        int actual = db.isUniqueID(id);
+
+        db.close();
+
+        //Set expected result
+        int expected = 0;
+
+        //test if result is what we expect
+        assertEquals(expected, actual);
+    }
+}
---
+++ Next Level/core/src/com/mygdx/nextlevel/JUnitTests/LevelsDBControllerTest.java
@@ -0,0 +1,4 @@
+package com.mygdx.nextlevel.JUnitTests;
+
+public class LevelsDBControllerTest {
+}
