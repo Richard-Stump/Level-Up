11c4eb5b457f07ee79149b9ad40f9d21ab9e4eac
Richard Stump <rnstump@purdue.edu>
2021-10-24 11:11:04 -0400

    Implemented actor placement in level editor

---
+++ Next Level/core/src/com/mygdx/nextlevel/screens/editor/EditorLevel.java
@@ -0,0 +1,36 @@
+package com.mygdx.nextlevel.screens.editor;
+
+import java.util.ArrayList;
+
+public class EditorLevel {
+    public int map[][];
+    public int width, height;
+    public ArrayList<EditorActor> actors;
+
+    public static int NONE = -1;
+
+    //creates an empty tile map
+    public EditorLevel(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        map = new int[width][height];
+
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                map[x][y] = NONE;
+            }
+        }
+
+        this.actors = new ArrayList<EditorActor>();
+        actors.add(new EditorActor(0, 0, 0));
+    }
+
+    public void setTile(int x, int y, int tileId) {
+        map[x][y] = tileId;
+    }
+
+    public void placeActor(float x, float y, int actorId) {
+        actors.add(new EditorActor(x, y, actorId));
+    }
+}
