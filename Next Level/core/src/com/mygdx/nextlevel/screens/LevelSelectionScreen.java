4673590bf56d4c052fde22bba3e595c5487d71c1
reeves34 <reeves34@purdue.edu>
2021-10-24 00:55:58 -0400

    basic layout for searching on LevelSelectionScreen

--- Next Level/core/src/com/mygdx/nextlevel/screens/LevelSelectionScreen.java
+++ Next Level/core/src/com/mygdx/nextlevel/screens/LevelSelectionScreen.java
@@ -7,17 +7,16 @@ package com.mygdx.nextlevel.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.utils.Align;
@@ -30,6 +29,8 @@ import com.mygdx.nextlevel.dbHandlers.CreatedLevelsDB;
 import com.mygdx.nextlevel.dbHandlers.DownloadedLevelsDB;
 import com.mygdx.nextlevel.enums.Difficulty;
 import com.mygdx.nextlevel.enums.Tag;
+import com.sun.java.accessibility.util.internal.ButtonTranslator;
+import com.sun.tools.javac.comp.Check;
 import jdk.internal.org.jline.utils.DiffHelper;
 import org.w3c.dom.Text;
 
@@ -52,6 +53,9 @@ public class LevelSelectionScreen implements Screen {
     public static final int STAGE_WIDTH = 1920 / 2;
     public static final int STAGE_HEIGHT = 1080 / 2;
 
+    private String selectedId;
+    private Label selectedLevel;
+
     public LevelSelectionScreen(NextLevel game) {
         this.game = game;
         atlas = new TextureAtlas(Gdx.files.internal("skin/neon-ui.atlas"));
@@ -69,6 +73,8 @@ public class LevelSelectionScreen implements Screen {
         stage = new Stage(viewport, batch);
 
         dbDownloaded = new DownloadedLevelsDB();
+        selectedLevel = new Label("Select a level", skin);
+        selectedId = "";
     }
 
     public void show() {
@@ -106,44 +112,90 @@ public class LevelSelectionScreen implements Screen {
         //row 2: empty placeholder, scrollable table with levels, sorting box
 
         //populate a table full of information
-        Table levelsTable = new Table();
-        //columns: {image}, {title, author, difficulty, tags}, {rating, play count}
+        VerticalGroup levelTable = new VerticalGroup();
+        //columns: {image}, {title, author, difficulty, tags}, empty space, {rating, play count}
         for (LevelInfo levelInfo: dbDownloaded.sortByTitle()) {
             String id = levelInfo.getId();
-            //levelsTable.add(getImage(id));
-            levelsTable.add(getTitleGroup(id));
-            levelsTable.add(getRatingGroup(id));
-            levelsTable.row();
+            HorizontalGroup levelGroup = new HorizontalGroup();
+
+            levelGroup.addListener(selectLevel(levelGroup, id));
+
+            //Image levelPreview = new Image();
+
+            //levelGroup.addActor(levelPreview);
+            levelGroup.addActor(getTitleGroup(id));
+            levelGroup.addActor(getRatingGroup(id));
+            levelGroup.padBottom(20);
+            levelTable.addActor(levelGroup);
         }
 
-        ScrollPane scrollPane = new ScrollPane(levelsTable, skin);
+        levelTable.top();
+        levelTable.padRight(20);
+
+        ScrollPane scrollPane = new ScrollPane(levelTable, skin);
         scrollPane.setForceScroll(true, true);
 
         //make the sorting and search thing on right side
-        VerticalGroup searchSortGroup = new VerticalGroup();
+        Table searchSortGroup = getSearchSortTable();
 
         table.add();
         table.add(scrollPane);
-        table.add(searchSortGroup);
+        table.add(searchSortGroup).top();
         table.row();
 
-        //row 3: empty placeholder, empty placeholder, play button
+        //row 3: empty placeholder, currently selected level, play button
 
         TextButton playButton = new TextButton("Play", skin);
-        playButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-
-            }
-        });
+        playButton.addListener(playLevel());
 
         table.add();
-        table.add();
+        table.add(selectedLevel).left();
         table.add(playButton);
 
         //end
 
         stage.addActor(table);
+    }
+
+    private Table getSearchSortTable() {
+        Table table = new Table();
+        Label searchLabel = new Label("Search:", skin);
+        TextField searchBar = new TextField("Search", skin);
+
+        Label diffLabel = new Label("Difficulty:", skin);
+        SelectBox<Difficulty> difficultyDropdown = new SelectBox<>(skin);
+        difficultyDropdown.setItems(Difficulty.class.getEnumConstants());
+
+        Label tagLabel = new Label("Tags:", skin);
+        ArrayList<CheckBox> tagCheckBoxes = new ArrayList<>();
+        for (Tag t: Tag.values()) {
+            tagCheckBoxes.add(new CheckBox(t.toString(), skin));
+        }
+
+        TextButton searchButton = new TextButton("Search", skin);
+        searchButton.addListener(new ClickListener() {
+            //apply search and update table
+        });
+
+        table.add(searchLabel);
+        table.row();
+        table.add(searchBar);
+        table.row();
+        table.add(diffLabel);
+        table.row();
+        table.add(difficultyDropdown);
+        table.row();
+        table.add(tagLabel);
+        table.row();
+
+        for (CheckBox cb: tagCheckBoxes) {
+            table.add(cb);
+            table.row();
+        }
+
+        table.add(searchButton);
+        table.row();
+        return table;
     }
 
     /**
@@ -164,6 +216,8 @@ public class LevelSelectionScreen implements Screen {
 
         VerticalGroup titleGroup = new VerticalGroup();
 
+        //definitely not the best way to do click listeners
+
         Label levelName = new Label(levelInfo.getTitle(), skin);
         Label author = new Label(levelInfo.getAuthor(), skin);
         String difficultyString = Difficulty.values()[levelInfo.getDifficulty()].getDisplayName();
@@ -172,6 +226,7 @@ public class LevelSelectionScreen implements Screen {
         titleGroup.addActor(levelName);
         titleGroup.addActor(author);
         titleGroup.addActor(difficultyAndTags);
+        titleGroup.left();
         return titleGroup;
     }
 
@@ -193,7 +248,32 @@ public class LevelSelectionScreen implements Screen {
         ratingGroup.addActor(rating);
         ratingGroup.addActor(playCount);
 
+        //make it go to the right side of the parent
+        ratingGroup.right();
+
         return ratingGroup;
+    }
+
+    private ClickListener selectLevel(final HorizontalGroup level, final String id) {
+        return new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //outline the selected level
+                selectedLevel.setText("Level Selected: " + dbDownloaded.searchByID(id).getTitle());
+                selectedId = id;
+            }
+        };
+    }
+
+    private ClickListener playLevel() {
+        return new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Should be playing: " + dbDownloaded.searchByID(selectedId).getTitle());
+                //TODO: open the game screen with the level that is selected
+
+            }
+        };
     }
 
 
