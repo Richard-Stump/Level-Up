f7fa6765656c94ca2b93b91f6958cafa3fa144ba
reeves34 <reeves34@purdue.edu>
2021-10-27 20:14:11 -0400

    fixed relative pathing for database tests

--- Next Level/core/src/com/mygdx/nextlevel/dbUtil/DBConnection.java
+++ Next Level/core/src/com/mygdx/nextlevel/dbUtil/DBConnection.java
@@ -5,7 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String CONNECTION = "jdbc:sqlite:core/src/com/mygdx/nextlevel/data/";
+    //private static final String CONNECTION = ;
+    private static final String CONNECTION = "jdbc:sqlite:" + System.getProperty("user.dir") + "/Next Level/core/src/com" +
+            "/mygdx/nextlevel/data/";
 
     public static Connection getConnection(String databaseName) throws SQLException {
         String connAll = CONNECTION;
