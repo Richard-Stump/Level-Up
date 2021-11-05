4dc9a001391ef7fb9463469f895ccd855911ef24
Nick Zogbi <61124554+nzogbi@users.noreply.github.com>
2021-11-04 23:32:58 -0400

    Camera Movement

--- Next Level/core/src/com/mygdx/nextlevel/TileMap.java
+++ Next Level/core/src/com/mygdx/nextlevel/TileMap.java
@@ -7,21 +7,63 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.objects.TextureMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.mygdx.nextlevel.actors.Player2;
 
 
 public class TileMap extends ApplicationAdapter{
     TiledMap tiledMap;
+    MapProperties tiledMapProperties;
     TiledMapRenderer tiledMapRenderer;
+    TiledMapTileLayer layer;
+
+    //Tile Map Properties
+    int mapWidth;
+    int mapHeight;
+    int tilePixelWidth;
+    int tilePixelHeight;
+
+    //Pixel Properties
+    int mapPixelWidth;
+    int mapPixelHeight;
+
+    //Camera Position
+    float xAxis = 0;
+    float yAxis = 0;
 
     public void create () {
         tiledMap = new TmxMapLoader().load("test3.tmx");
+        tiledMapProperties = tiledMap.getProperties();
+        layer = (TiledMapTileLayer) tiledMap.getLayers().get(0);
+
+        mapWidth = tiledMapProperties.get("width", Integer.class);
+        mapHeight = tiledMapProperties.get("height", Integer.class);
+        tilePixelWidth = tiledMapProperties.get("tilewidth", Integer.class);
+        tilePixelHeight = tiledMapProperties.get("tileheight", Integer.class);
+
+        mapPixelWidth = mapWidth * tilePixelWidth;
+        mapPixelHeight = mapHeight * tilePixelHeight;
+
+        System.out.println("Testing");
+        System.out.println(mapWidth);
+        System.out.println(mapHeight);
+        System.out.println(tilePixelWidth);
+        System.out.println(tilePixelHeight);
+
+        System.out.println(Gdx.graphics.getWidth()/2f);
+        System.out.println(Gdx.graphics.getWidth()/32f);
+        System.out.println(mapPixelWidth);
+        System.out.println(mapPixelHeight);
+        System.out.println("Testing");
+
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1.0f/32.0f);
     }
 
@@ -40,6 +82,18 @@ public class TileMap extends ApplicationAdapter{
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        float screenWidth = Gdx.graphics.getWidth()/32f;
+        if (player.getX() <= screenWidth/2f) {
+            xAxis = screenWidth/2f;
+        } else if (mapWidth - player.getX() <= screenWidth/2f) {
+            xAxis = mapWidth - screenWidth/2f;
+        } else {
+            xAxis = player.getX();
+        }
+        yAxis = Gdx.graphics.getHeight()/2f/32f;
+
+        camera.position.x = xAxis;
+        camera.position.y = yAxis;
         camera.update();
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
