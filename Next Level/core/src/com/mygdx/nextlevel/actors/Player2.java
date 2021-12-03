0d2a98aed81a945f4b5345379c18682a4adb20a2
Richard Stump <rnstump@purdue.edu>
2021-12-03 03:54:23 -0500

    Begin connecting the level editor to the game screen

--- Next Level/core/src/com/mygdx/nextlevel/actors/Player2.java
+++ Next Level/core/src/com/mygdx/nextlevel/actors/Player2.java
@@ -74,8 +74,6 @@ public class Player2 extends Actor2 {
 //    private int condition = 5;
 //    private int condition2 = 2;
 
-    @Property(group="Test", displayName = "Test String") public String strTest = "Testing";
-    @Property(group="Test", displayName = "Test Enum")  public Difficulty difficulty = Difficulty.HARD;
     @Property(group="Movement", displayName="Default Speed") public float defaultMoveSpeed = 12.0f;
     @Property(group="Movement", displayName="Default Jump") public float defaultJump = 13.0f;
 
