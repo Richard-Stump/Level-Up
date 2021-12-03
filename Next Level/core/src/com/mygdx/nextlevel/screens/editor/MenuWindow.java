286661d8b1ced4da47106dc5cbf1dfa7b552a9af
Joseph Chen <chen3242@purdue.edu>
2021-12-03 16:27:26 -0500

    Working on asset window

--- Next Level/core/src/com/mygdx/nextlevel/screens/editor/MenuWindow.java
+++ Next Level/core/src/com/mygdx/nextlevel/screens/editor/MenuWindow.java
@@ -28,6 +28,7 @@ public class MenuWindow extends VisWindow {
     Button saveButton;
     Button testButton;
     Button publishButton;
+    Button assetButton;
 
     EditorLevel level;
     EditLevelScreen screen;
@@ -53,6 +54,9 @@ public class MenuWindow extends VisWindow {
         table.add(testButton).width(BUTTON_WIDTH).pad(BUTTON_PADDING).fillY();
         publishButton = new TextButton("Publish", VisUI.getSkin());
         table.add(publishButton).width(BUTTON_WIDTH).pad(BUTTON_PADDING).fillY();
+        assetButton = new TextButton("Custom\nAssets", VisUI.getSkin());
+        table.add(assetButton).width(BUTTON_WIDTH).pad(BUTTON_PADDING).fillY();
+
 
         final EditorLevel lev = level;
 
@@ -95,7 +99,7 @@ public class MenuWindow extends VisWindow {
 
         add(table).fill();
 
-        int numButtons = 4;
+        int numButtons = 5;
         float width = 50 + BUTTON_WIDTH * numButtons + BUTTON_PADDING * numButtons;
         float x = EditLevelScreen.STAGE_WIDTH - width;
         float y = EditLevelScreen.STAGE_HEIGHT;
